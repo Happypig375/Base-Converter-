e78bdccb88277f5707b89a761f4f6904e71d25b4
Happypig375 <hadrianwttang@outlook.com>
2016-06-28 21:35:11 +0800

    Basic Converter to Decimal

---
+++ Base Converter Sharp/Base Converter Sharp/Converter.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Converter
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Input = New System.Windows.Forms.TextBox()
+        Me.Output = New System.Windows.Forms.TextBox()
+        Me.InputBase = New System.Windows.Forms.ComboBox()
+        Me.OutputBase = New System.Windows.Forms.ComboBox()
+        Me.InputLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Input
+        '
+        Me.Input.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.Input.Location = New System.Drawing.Point(237, 12)
+        Me.Input.Name = "Input"
+        Me.Input.Size = New System.Drawing.Size(177, 20)
+        Me.Input.TabIndex = 0
+        '
+        'Output
+        '
+        Me.Output.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.Output.Location = New System.Drawing.Point(237, 38)
+        Me.Output.Name = "Output"
+        Me.Output.Size = New System.Drawing.Size(177, 20)
+        Me.Output.TabIndex = 1
+        '
+        'InputBase
+        '
+        Me.InputBase.FormattingEnabled = True
+        Me.InputBase.Location = New System.Drawing.Point(49, 12)
+        Me.InputBase.Name = "InputBase"
+        Me.InputBase.Size = New System.Drawing.Size(182, 21)
+        Me.InputBase.TabIndex = 2
+        '
+        'OutputBase
+        '
+        Me.OutputBase.FormattingEnabled = True
+        Me.OutputBase.Location = New System.Drawing.Point(48, 38)
+        Me.OutputBase.Name = "OutputBase"
+        Me.OutputBase.Size = New System.Drawing.Size(183, 21)
+        Me.OutputBase.TabIndex = 3
+        '
+        'InputLabel
+        '
+        Me.InputLabel.AutoSize = True
+        Me.InputLabel.Location = New System.Drawing.Point(12, 15)
+        Me.InputLabel.Name = "InputLabel"
+        Me.InputLabel.Size = New System.Drawing.Size(31, 13)
+        Me.InputLabel.TabIndex = 4
+        Me.InputLabel.Text = "Input"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(4, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Output"
+        '
+        'Converter
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(426, 73)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.InputLabel)
+        Me.Controls.Add(Me.OutputBase)
+        Me.Controls.Add(Me.InputBase)
+        Me.Controls.Add(Me.Output)
+        Me.Controls.Add(Me.Input)
+        Me.MaximizeBox = False
+        Me.Name = "Converter"
+        Me.Text = "Base Converter#"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Input As TextBox
+    Friend WithEvents Output As TextBox
+    Friend WithEvents InputBase As ComboBox
+    Friend WithEvents OutputBase As ComboBox
+    Friend WithEvents InputLabel As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Base Converter Sharp/Base Converter Sharp/Converter.vb
@@ -0,0 +1,113 @@
+﻿Public Class Converter
+
+    Public Enum Base As Byte
+        Unary = 1
+        Binary
+        Ternary
+        Quaternary
+        Quinary
+        Senary
+        Septnary
+        Octal
+        Nonary
+        [Decimal]
+        Undecimal
+        Duodecimal
+        Tridecimal
+        Tetradecimal
+        Pentadecimal
+        Hexadecimal
+        Septendecimal
+        Octodecimal
+        Nonadecimal
+        Vigesimal
+        Unovigesimal
+        Duovigesimal
+        Triovigesimal
+        Quadrovigesimal
+        Pentavigesimal
+        Hexavigesimal
+        Heptovigesimal
+        Octovigesimal
+        Novovigesimal
+        Trigesimal
+        Unotrigesimal
+        Duotrigesimal
+        Triotrigesimal
+        Quadrotrigesimal
+        Pentatrigesimal
+        Hexatrigesimal
+        Heptotrigesimal
+        Octotrigesimal
+        Novotrigesimal
+        Quadragesimal
+        Unoquadragesimal
+        Duoquadragesimal
+        Trioquadragesimal
+        Quadroquadragesimal
+        Pentaquadragesimal
+        Hexaquadragesimal
+        Heptoquadragesimal
+        Octoquadragesimal
+        Novoquadragesimal
+        Quinquagesimal
+        Unoquinquagesimal
+        Duoquinquagesimal
+        Trioquinquagesimal
+        Quadroquinquagesimal
+        Pentaquinquagesimal
+        Hexaquinquagesimal
+        Heptoquinquagesimal
+        Octoquinquagesimal
+        Novoquinquagesimal
+        Sexagesimal
+        Unosexagesimal
+        Duosexagesimal
+    End Enum
+
+    Public Shared Function Convert([In] As Base, Value As String, Optional Out As Base = Base.Decimal) As String
+        Dim TempDecimal As String
+        If [In] = Base.Decimal Then
+            TempDecimal = Value
+        ElseIf [In] = Base.Unary Then
+            TempDecimal = Value.Count.ToString
+        Else
+            For Chars As UInteger = 0 To Value.Count + 1
+                Value = Value.Reverse
+
+            Next
+        End If
+    End Function
+
+    Public Shared Function ToValue(Character As Char) As Byte
+        Select Case Character
+            Case "0" To "9"
+                Return AscW(Character)
+            Case "A" To "Z"
+                Return AscW(Character) - AscW("A"c) + 10
+        End Select
+    End Function
+
+    Private Sub UpdateOutput()
+        Output.Text = Convert(InputBase.SelectedIndex + 1, Input.Text, OutputBase.SelectedIndex + 1)
+    End Sub
+
+    Private Sub Converter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Base As Byte = 1 To 62
+            InputBase.Items.Add([Enum].GetName(GetType(Base), Base) & " (Base " & Base & ")"c)
+        Next
+        OutputBase.Items.AddRange(InputBase.Items.Cast(Of String).ToArray)
+    End Sub
+
+    Private Sub InputBase_SelectedIndexChanged(sender As Object, e As EventArgs) Handles InputBase.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Input_TextChanged(sender As Object, e As EventArgs) Handles Input.TextChanged
+
+    End Sub
+
+    Private Sub OutputBase_SelectedIndexChanged(sender As Object, e As EventArgs) Handles OutputBase.SelectedIndexChanged
+
+    End Sub
+End Class
