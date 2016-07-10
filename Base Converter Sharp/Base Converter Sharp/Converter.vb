0fc296dd0413cef2bea6ab3a00066e39f2e41882
Happypig375 <hadrianwttang@outlook.com>
2016-07-10 22:32:30 +0800

    IntX and stuff

--- Base Converter Sharp/Base Converter Sharp/Converter.vb
+++ Base Converter Sharp/Base Converter Sharp/Converter.vb
@@ -66,32 +66,77 @@
     End Enum
 
     Public Shared Function Convert([In] As Base, Value As String, Optional Out As Base = Base.Decimal) As String
-        Dim TempDecimal As String
+#If False Then
+        Array.ForEach(Value.ToCharArray, Sub(c As Char) If Not Char.IsLetterOrDigit(
+        c) Then Throw New ArgumentException("Value", "The string contains nonalphanumeric characters."))
+        Dim TempDecimal As IntX
         If [In] = Base.Decimal Then
             TempDecimal = Value
         ElseIf [In] = Base.Unary Then
             TempDecimal = Value.Count.ToString
         Else
-            For Chars As UInteger = 0 To Value.Count + 1
-                Value = Value.Reverse
-
+            Value = Value.Reverse
+            For Each Character As Char In Value
+                Add(TempDecimal, Val(Character) * [In] ^ Value.IndexOf(Character))
+                ' Still undone
+                ' Working on here
             Next
         End If
+#End If
+        Dim Temp As IntX = IntX.Parse(Value, [In])
     End Function
 
     Public Shared Function ToValue(Character As Char) As Byte
         Select Case Character
             Case "0"c To "9"c
-                Return AscW(Character)
+                Return Val(Character)
             Case "A"c To "Z"c, "a"c To "z"c
-                Return AscW(Character) - AscW("A"c) + If(Char.IsLower(Character), 10, 36)
+                Return AscW(Character) + If(Char.IsUpper(Character), 10 - AscW("A"c), 37 - AscW("a"c))
             Case Else
-                Throw New ArgumentOutOfRangeException("Character", "Character must be either 0 to 9, a to z or A to Z.")
+                Throw New ArgumentOutOfRangeException("Character", Character, "Character must be either 0 to 9, a to z or A to Z.")
         End Select
     End Function
 
+    Public Shared Function ToChar(Value As Byte) As Char
+        Select Case Value
+            Case 0 To 9
+                Return Value.ToString
+            Case 10 To 62
+                Return ChrW(Value - If(Value <= 36, 10 - AscW("A"c), 37 - AscW("a"c)))
+            Case Else
+                Throw New ArgumentOutOfRangeException("Value", Value, "Value must be between 0 to 62.")
+        End Select
+    End Function
+
+    Public Shared Sub Add(ByRef Subject As String, Value As String)
+        Array.ForEach(Subject.ToCharArray, Sub(c As Char) If Not Char.IsDigit(
+        c) Then Throw New ArgumentException("The string contains nonnumeric characters.", "Value"))
+        Array.ForEach(Value.ToCharArray, Sub(c As Char) If Not Char.IsDigit(
+        c) Then Throw New ArgumentException("The string contains nonnumeric characters.", "Value"))
+        Subject = Subject.Reverse
+        Value = Value.Reverse
+        For Each Digit As Char In Value
+
+        Next
+    End Sub
+
+    Private Function Add(x As Integer, y As Integer) As Integer
+        ' Iterate till there is no carry  
+        While y <> 0
+            ' carry now contains common set bits of x and y
+            Dim carry As Integer = x And y
+
+            ' Sum of bits of x and y where at least one of the bits is not set
+            x = x Xor y
+
+            ' Carry is shifted by one so that adding it to x gives the required sum
+            y = carry << 1
+        End While
+        Return x
+    End Function
+
     Private Sub UpdateOutput()
-        Output.Text = Convert(InputBase.SelectedIndex + 1, Input.Text, OutputBase.SelectedIndex + 1)
+        'Output.Text = Convert(InputBase.SelectedIndex + 1, Input.Text, OutputBase.SelectedIndex + 1)
     End Sub
 
     Private Sub Converter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -99,6 +144,8 @@
             InputBase.Items.Add([Enum].GetName(GetType(Base), Base) & " (Base " & Base & ")"c)
         Next
         OutputBase.Items.AddRange(InputBase.Items.Cast(Of String).ToArray)
+        ToChar(ToValue("a"))
+        End
     End Sub
 
     Private Sub InputBase_SelectedIndexChanged(sender As Object, e As EventArgs) Handles InputBase.SelectedIndexChanged
---
+++ Base Converter Sharp/Base Converter Sharp/FloatX.vb
@@ -0,0 +1,84 @@
+﻿Public Class FloatX
+    Implements ICloneable
+    Public Property Number As IntX
+    Public Property Exponent As Integer
+#Region "Initializers"
+    Sub New()
+        Number = New IntX
+        Exponent = New Integer
+    End Sub
+    Sub New(value As Integer)
+        Number = New IntX(value)
+        Exponent = New Integer
+    End Sub
+    Sub New(value As UInteger)
+        Number = New IntX(value)
+        Exponent = New Integer
+    End Sub
+    Sub New(value As Long)
+        Number = New IntX(value)
+        Exponent = New Integer
+    End Sub
+    Sub New(value As ULong)
+        Number = New IntX(value)
+        Exponent = New Integer
+    End Sub
+    Sub New(value As IntX)
+        Number = New IntX(value)
+        Exponent = New Integer
+    End Sub
+    Sub New(value As FloatX)
+        Number = New IntX(value.Number)
+        Exponent = value.Exponent
+    End Sub
+    Sub New(value As IntX, exponent As Integer)
+        Number = New IntX(value)
+        Me.Exponent = exponent
+    End Sub
+#End Region
+    Public Shared Operator +(Num1 As FloatX, Num2 As FloatX)
+        EqualifyExponents(Num1, Num2)
+        Return New FloatX(Num1.Number + Num2.Number, Num1.Exponent)
+    End Operator
+    Public Shared Function IsNumeric(value As Object) As Boolean
+        Return TypeOf value Is Byte OrElse
+               TypeOf value Is Int16 OrElse
+               TypeOf value Is Int32 OrElse
+               TypeOf value Is Int64 OrElse
+               TypeOf value Is SByte OrElse
+               TypeOf value Is UInt16 OrElse
+               TypeOf value Is UInt32 OrElse
+               TypeOf value Is UInt64 OrElse
+               TypeOf value Is IntX OrElse
+               TypeOf value Is Decimal OrElse
+               TypeOf value Is Double OrElse
+               TypeOf value Is Single OrElse
+               TypeOf value Is FloatX
+    End Function
+    Function Clone() As Object Implements ICloneable.Clone
+        Return New FloatX(Me)
+    End Function
+    Sub Normalize()
+        Number.Normalize()
+        While Number Mod 10 = 0
+            Exponent += 1
+            Number /= 10
+        End While
+    End Sub
+    Shared Sub EqualifyExponents(ByRef Num1 As FloatX, ByRef Num2 As FloatX)
+        If Max(Num1.Exponent, Num2.Exponent) = Num1.Exponent Then
+            Num1.Number *= 10 * (New IntX(Num1.Exponent) - Num2.Exponent)
+            Num1.Exponent = Num2.Exponent
+        Else
+            Num2.Number *= 10 * (New IntX(Num2.Exponent) - Num1.Exponent)
+            Num2.Exponent = Num1.Exponent
+        End If
+    End Sub
+    Shared Function Parse(Input As String) As FloatX
+        If Input.Contains("."c) Then
+            Throw New NotImplementedException
+        Else
+            Return New FloatX(IntX.Parse(Input))
+        End If
+    End Function
+End Class
